f1dcc75e46dc34ae6f32e38342e9c50e7d2978cd
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-12-14 09:30:17 -0500

    ciao4.13 setup changes

--- setup.py
+++ setup.py
@@ -5,15 +5,6 @@ import sys
 
 assert "ASCDS_INSTALL" in os.environ, "Please run this after CIAO has been setup"
 
-#
-# I need the pythonX.Y version for the purelib's.  Can't find a way 
-# to pass in so that it gets picked up by setup.cfg so we just set
-# and env variable and that works.
-#
-
-ver = sys.version_info
-os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
-
 
 scripts = ["image_tempmap","make_mkwarf_map","multi_spec"]
 params = [ x+".par" for x in scripts]
@@ -22,10 +13,10 @@ docs = [ x+".xml" for x in scripts if os.path.exists(x+".xml")]
 
 from distutils.core import setup
 setup( name='TemperatureMaps',
-        version='0.0.1',
+        version='4.13.0',
         description='Temperature maps scripts',
-        author='Anonymous',
-        author_email='WhoDat@cfa.harvard.edu',
+        author='Kenny Glotfelty',
+        author_email='glotfeltyk@si.edu',
         url='https://github.com/kglotfelty/TemperatureMaps/',
         scripts = scripts,
         data_files = [ ("param", params ), ("share/doc/xml", docs ) ]
